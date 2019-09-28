5e1a684a571746d38b556b0341f9ae8799d83e9e
Vegard Brenden <vegard@crude.no>
2019-09-28 19:44:34 +0200

    Added driver via athom CLI

---
+++ drivers/airthings-wave-plus/driver.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Homey = require('homey');
+
+class MyDriver extends Homey.Driver {
+	
+	onInit() {
+		this.log('MyDriver has been inited');
+	}
+
+	// This method is called when a user is adding a device
+  	// and the 'list_devices' view is called
+	onPairListDevices(data, callback) {
+
+		callback(null, [
+			{
+				name: 'Foo Device',
+				data: {
+					id: 'abcd1234'
+				}
+			}
+		]);
+		return;
+
+        Homey.app.discoverDevices(this)
+            .then(devices => {
+                callback(null, devices);
+            })
+            .catch(error => {
+                callback(new Error('Cannot get devices:' + error));
+            });
+    }
+	
+}
+
+module.exports = MyDriver;
