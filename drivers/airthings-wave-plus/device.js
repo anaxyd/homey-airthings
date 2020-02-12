2e5db1e8d409c651a8feadb54763921cba034cbf
Marco Laurenzano <ml@laurenzano.it>
2020-02-12 23:29:24 +0100

    Completed luminance support for all waves Improved connection and added connection release upon failure Small changes to defaults and radon max limits Refactoring of device type specific functions

--- drivers/airthings-wave-plus/device.js
+++ drivers/airthings-wave-plus/device.js
@@ -5,50 +5,58 @@ const Homey = require('homey');
 class WavePlusDevice extends Homey.Device {
 	
 	onInit() {
-		this.log('WavePlusDevice has been inited');
+		this.log('WavePlusDevice has been initialized');
 
 		const settings = this.getSettings();
 		const pollInterval = settings.pollInterval;
 		this.log(pollInterval);
 		const POLL_INTERVAL = 1000 * 60 * pollInterval; // default 30 minutes
 
+		this.per = undefined;
+
+		// migrate from previous version of the driver
+		if(!this.hasCapability("measure_luminance")) {
+			this.log("Missing luminance. Adding to the current device");
+			this.addCapability("measure_luminance");
+		}
+
         // Run poll at init
         this.poll();
 
 		setInterval(this.poll.bind(this), POLL_INTERVAL);
-
 	}
 
 	poll() {
 
-		this.log('Polling device...');
+		this.log('Polling device');
 
 		const macAddress = this.getData().uuid;
 		const settings = this.getSettings();
 		const pollTimeout = settings.pollTimeout;
 
-		Homey.app.getWavePlusValues(macAddress, pollTimeout)
-			.then(result => {
-				this.log(result)
-
+		Homey.app.getSemaphore().wait(async () => {
+			try {
+				let result = await Homey.app.getValuesFromDevice(this, macAddress, pollTimeout); 
+				
 				this.setCapabilityValue("measure_co2", result.co2);
 				this.setCapabilityValue("measure_pressure", result.pressure);
 				this.setCapabilityValue("measure_humidity", result.humidity);
 				this.setCapabilityValue("measure_temperature", result.temperature);
 				this.setCapabilityValue("measure_voc", result.voc);
 				this.setCapabilityValue("measure_radon", result.shortTermRadon);
-
-				this.log("Airthings Wave Plus sensor values updated");
-
-				return Promise.resolve();
-
-			})
-			.catch(error => {
-				new Error('Cannot get value:' + error);
-			});
-	}
-
+				this.setCapabilityValue("measure_luminance", result.light);
 	
+				Homey.app.getSemaphore().signal();
+				this.log(result);
+	
+			} catch(error) {
+
+				Homey.app.getSemaphore().signal();
+				this.log(error);
+			}
+		})
+		
+	}
 }
 
 module.exports = WavePlusDevice;
--- drivers/airthings-wave/driver.js
+++ drivers/airthings-wave/driver.js
@@ -5,14 +5,14 @@ const Homey = require('homey');
 class WaveDriver extends Homey.Driver {
 	
 	onInit() {
-		this.log('WaveDriver has been inited');
+		this.deviceType = 'Wave';
 	}
 
 	// This method is called when a user is adding a device
   	// and the 'list_devices' view is called
 	onPairListDevices(data, callback) {
 
-		Homey.app.discoverWaveDevices(this)
+		Homey.app.discoverDevices(this)
 			.then(devices => {
 				this.log(devices)
 				callback(null, devices);
